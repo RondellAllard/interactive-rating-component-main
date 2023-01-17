edbd9da929d8be7264ca350010f4d45820438f4b
Pierre Allard <rondellallard@gmail.com>
2023-01-17 14:10:51 -0400

    JS complete
    JS to change the card on submitting  completed.

---
+++ card.js
@@ -0,0 +1,17 @@
+// get the elements
+const submitBtn = document.querySelector("#feedback-submit");
+const feedbackCard = document.querySelector("#card-contents");
+const thankYouMessage = document.querySelector("#post-rating");
+const valueP = document.querySelector("#selection-notification");
+
+// listen for click on submit button
+submitBtn.addEventListener("click", event => {
+  event.preventDefault();
+  let ratingValue = document.querySelector("input[name='rating']:checked").value;
+  // hide the hidden div
+  feedbackCard.style.display = "none";
+  // show the visible div
+  thankYouMessage.style.display = "flex";
+  // set the value of the p tag
+  valueP.innerHTML = `You ${ratingValue}  selected out of 5`;
+});
